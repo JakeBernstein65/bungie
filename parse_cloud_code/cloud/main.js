88eebaf9789137df65b0e8876cacf4dfbb18089c
Jake Bernstein <jakedbernstein@gmail.com>
2015-09-29 21:17:43 -0400

    basic version of extension is live and working

--- parse_cloud_code/cloud/main.js
+++ parse_cloud_code/cloud/main.js
@@ -1,20 +1,3 @@
-Parse.Cloud.afterSave("Text", function(request) {
-var selectedText = request.object.get("text");
-
-	//add the below method to the cloud job
- //   Parse.Cloud.httpRequest({
-	//   method: "POST",
-	//   url: "https://ACb557922e1bb38ab5233c76d23df8a525:c4b9795ee9c63a342565d80d5f813cfd@api.twilio.com/2010-04-01/Accounts/ACb557922e1bb38ab5233c76d23df8a525/SMS/Messages.json",
-	//   body: {
-	//      From:"+15162462585",
-	//      To: "+15163189812",
-	//      Body: "-> \n" + selectedText
-	//    }
-	// });
-});
-
-
-
 Parse.Cloud.job("sendText", function(request, status) {
 
    Parse.Cloud.httpRequest({
@@ -34,7 +17,7 @@ Parse.Cloud.job("sendText", function(request, status) {
 
 	          	 var currentTime = new Date().getTime();
 //seconds in a day = 86400000
-				   if ((currentResult.timeStamp + 12) <= currentTime){
+				   if ((currentResult.timeStamp + 7200000) <= currentTime){
 				    	currentResult.timesSent ++;
 				    	currentResult.timeStamp = currentTime;
 				    	console.log(" \n \n currentTime = " + currentTime);
@@ -74,51 +57,9 @@ Parse.Cloud.job("sendText", function(request, status) {
 					     	console.log(" \n \n Put response = " + JSON.stringify(response));
 					     });
 					    }
-            }
-         }
+                  }
+             }
     });
-
-	  //   var currentTime = new Date().getTime();
-
-	  //   if (blank.timeStap + 86400000 <== currentTime){
-	  //   	//increment timesSent and timeStamp
-	  //   	blank.timesSent ++;
-	  //   	blank.timeStamp = currentTime;
-
-	  //   	Parse.Cloud.httpRequest({
-		 //  	  method: 'Put',
-		 //      url: "https://api.parse.com/1/classes/Text/" + blank.objectId, blank,
-		 //      headers: {
-		 //        'X-Parse-Application-Id':'DQvjcrwLM1ctu4Wri3o3OEi5tLe8tvtqeCCU5egq',
-		 //        'X-Parse-REST-API-Key':'fDIml4hbYCWOj8B6v74ig7nNHqgESeGjB3XNXj3h',
-		 //        'Content-Type':'application/json'
-		 //       }
-		 //    });
-
-
-	  //   	//send actual text
-	  //   	Parse.Cloud.httpRequest({
-			//   method: "POST",
-			//   url: "https://ACb557922e1bb38ab5233c76d23df8a525:c4b9795ee9c63a342565d80d5f813cfd@api.twilio.com/2010-04-01/Accounts/ACb557922e1bb38ab5233c76d23df8a525/SMS/Messages.json",
-			//   body: {
-			//      From:"+15162462585",
-			//      To: "+15163189812",
-			//      Body: "-> \n" + selectedText
-			//    }
-			// });
-	  //   }
-
-	  //   if (blank.timeStap >== 7){
-	  //   	Parse.Cloud.httpRequest({
-		 //  	  method: 'Delete',
-		 //      url: "https://api.parse.com/1/classes/Text/" + blank.objectId,
-		 //      headers: {
-		 //        'X-Parse-Application-Id':'DQvjcrwLM1ctu4Wri3o3OEi5tLe8tvtqeCCU5egq',
-		 //        'X-Parse-REST-API-Key':'fDIml4hbYCWOj8B6v74ig7nNHqgESeGjB3XNXj3h',
-		 //        'Content-Type':'application/json'
-		 //       }
-		 //    });
-	  //   }
 });
 
 
